e4139e02b9364d969018052616618d74f5df7b6e
ehhhsandy <ehhhsandy@users.noreply.github.com>
2023-01-07 16:50:05 -0500

    created tab boxes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,11 +8,11 @@ const NavBar = () => {
         <div className={styles.NavBarStyled}>
             <img className={styles.Logo} src={logo} alt="Penguin with tie" />
             <ul className={styles.TabsList}>
-                <li><div className={styles.Tab}>About</div></li>
-                <li><div className={styles.Tab}>Experience</div></li>
-                <li><div className={styles.Tab}>Projects</div></li>
-                <li><div className={styles.Tab}>Education</div></li>
-                <li><div className={styles.Tab}>Contact</div></li>
+                <li><div className={styles.TabBox}><div className={styles.Tab}>About</div></div></li>
+                <li><div className={styles.TabBox}><div className={styles.Tab}>Experience</div></div></li>
+                <li><div className={styles.TabBox}><div className={styles.Tab}>Projects</div></div></li>
+                <li><div className={styles.TabBox}><div className={styles.Tab}>Education</div></div></li>
+                <li><div className={styles.TabBox}><div className={styles.Tab}>Contact</div></div></li>
             </ul>
         </div>
     )
