2fa9819aa51212b8351a14c5950185b730e98e44
sandynguyen8928 <sandynguyen8928@gmail.com>
2023-01-27 22:53:35 -0500

    changed font type

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -4,7 +4,7 @@ import styles from "./PageTitle.module.scss";
 const PageTitle = (props: any) => {
 
     return (
-        <div>
+        <div className={styles.PageTitleStyled}>
             <div className={styles.BigTitle}>{props.title}</div>
             <div className={styles.SmallTitle}>{props.title}</div>
         </div>
