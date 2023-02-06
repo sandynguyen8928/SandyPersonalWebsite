8e96d38e297d68b80578bfd7233fc9d79a9f24db
sandynguyen8928 <sandynguyen8928@gmail.com>
2023-02-06 00:06:47 -0500

    added links

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         <div className={styles.HomePageStyled}>
             <div className={styles.RowOne}>
                 <div className={styles.LeftChild}>
-                    <div className={styles.Name}>Sandy <br></br> Nguyen</div>
+                    <div className={styles.Name}>Sandy <br/> Nguyen</div>
                     <div className={styles.Title}>Full Stack Developer</div>
                     <div className={styles.Description}>I am passionate about <b>human interaction</b> and <b>communication</b>.
                         I enjoy the challenge of building products with the goal of <b>optimizing user experience.</b></div>
@@ -19,14 +19,20 @@ const HomePage = () => {
 
                 <div className={styles.RightChild}>
                     <img className={styles.Profile} src={profile} alt="My face" />
-                    <div className={styles.ProfileBack}></div>
+                    <div className={styles.ProfileBack}/>
                 </div>
             </div>
 
             <div className={styles.BottomChild}>
-                <img className={styles.Socials} src={linkedin} alt="Linkedin logo" />
-                <img className={styles.Socials} src={github} alt="Github logo" />
-                <img className={styles.Socials} src={devpost} alt="Devpost logo" />
+                <a href="https://www.linkedin.com/in/sandynguyen8928/" target="_blank" rel="noopener noreferrer">
+                    <img className={styles.Socials} src={linkedin} alt="Linkedin logo" />
+                </a>
+                <a href="https://github.com/sandynguyen8928" target="_blank" rel="noopener noreferrer">
+                    <img className={styles.Socials} src={github} alt="Github logo" />
+                </a>
+                <a href="https://devpost.com/sandynguyen8928?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" target="_blank" rel="noopener noreferrer">
+                    <img className={styles.Socials} src={devpost} alt="Devpost logo" />
+                </a>
             </div>
 
             <div className={styles.BottomChild}>
